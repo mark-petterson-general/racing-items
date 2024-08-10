9ee008756611a88e8167a11d343cb13d049aa83f
mark.petterson <mpette200@gmail.com>
2024-08-10 11:23:25 +0100

    adjusted frame rate

---
+++ reward_funcs/reward_12b.py
@@ -0,0 +1,91 @@
+class Ref:
+    history = {}
+
+
+def reward_function(params):
+    '''
+    Reward for staying close to centre line. Centreline reward
+    to have an s-shaped curve based on logistic function.
+
+    Reward for making more progress, for example by cutting corners.
+    For this, the term effective speed has been devised which is computed
+    based on the progress percentage, the track length and the total
+    number of steps completed. The effective speed is the speed that it
+    would have been expected to travel at to have progressed that far
+    around the track.
+
+    The progress reward can be raised to a power to help reward faster
+    times. For example if the track is completed faster this can often
+    lead to less reward because there are fewer steps in which to accumulate
+    rewards. Raising to a power (eg. squaring) can help with this.
+
+    Punish if at any time in the previous 20 steps it did not keep all
+    the wheels on the track. This is done via the stay_on_reward.
+
+    The centre, progress and stay_on_reward are multiplied together.
+
+    '''
+
+    const_e = 2.718281828
+
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    progress = params['progress']  # percentage
+    nsteps = int(params['steps'])  # seems to be approx 1/6 second
+    track_length = params['track_length']  # metres
+    all_wheels_on_track = params['all_wheels_on_track']
+
+    # Setup history
+    history = Ref.history
+    if nsteps <= 1:
+        history.clear()
+    history[nsteps] = {
+        'progress': progress,
+        'all_wheels_on_track': all_wheels_on_track,
+    }
+
+    # Overall scaling
+    scaling_wt = 2.0
+
+    # Raise progress to this exponent
+    progress_power = 1.7
+
+    # Timestep assumed for progress computations
+    steps_per_sec = 15
+
+    # Logistic function parameters
+    # https://en.wikipedia.org/wiki/Logistic_function
+    # for following center
+    k = 8  # steepness
+    zone_width = 0.7  # normalized to track_width so 1.0 is at edge of track
+
+    # logistic function for centerline
+    x0 = zone_width * track_width / 2
+    centreline_reward = 1 - 1 / (
+        1 + const_e ** (k * (x0 - distance_from_center)))
+    # never zero
+    centreline_reward = max(0.001, centreline_reward)
+
+    # progress reward is effective speed
+    # where effective speed is computed by converting the
+    # progress into a metres per second value based on the track length
+    progress_reward = 1.0  # default
+    # avoid divide by zero error
+    if nsteps > 0:
+        effective_speed = progress / 100 * track_length * steps_per_sec / nsteps
+        # normalize so reward is 1.0 if effective speed is 2.8 m/s
+        progress_reward = effective_speed / 2.8
+
+    # reward for staying on the track
+    stay_on_reward = 1.0  # default
+    steps_in_history = [i for i in range(nsteps - 20, nsteps)
+                        if i in history]
+    if any(not history[i]['all_wheels_on_track'] for i in steps_in_history):
+        # went off the track
+        stay_on_reward = 0.001
+
+    # Non-linear so more progress is rewarded
+    return scaling_wt * centreline_reward \
+        * progress_reward ** progress_power \
+        * stay_on_reward
